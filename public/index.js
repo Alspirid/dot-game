8dc96a0dcfdd4a4eab4541b917c02ddb85ed8f62
Alexey Spiridonov <alexey@datacoral.co>
2019-03-10 23:54:14 -0700

    Refactored circle colors by adding random gradient, deleted playground background

--- public/index.js
+++ public/index.js
@@ -8,58 +8,70 @@ class Utils {
     Math.round((Math.random() * (max - min) + min) / 10) * 10
 
   static removeEl = el => el.parentNode.removeChild(el)
+  
+  static updateToggleButton = (button, label, classToAdd, classToRemove) => {
+    button.innerHTML = label
+    button.classList.add(classToAdd)
+    button.classList.remove(classToRemove)
+  }
 }
 
+class Gradient {
+  static GRADIENT_TYPE = 'radial-gradient'
+  static GRADIENTS = [
+    '(#f26080, #ed2f59, #f2073b)', // red
+    '(#95bcf9, #4b89ed, #0960ed)', // blue
+    '(#68e863, #3aa536, #047201)', // green
+    '(#d387ed, #9e3dbf, #680e87)', // violet
+    '(#f9e086, #f4c829, #f9c300)', // yellow
+  ]
+  static getRandomGradient = () => {
+    return this.GRADIENT_TYPE + this.GRADIENTS[Math.floor(Math.random() * this.GRADIENTS.length)]
+  }
+}
 
 class Game {
-
   state = {
-      score: 0,
-      isRunning: false,
-      interval: 0,
-      timer: 1000,
-      colors: ['#556270', '#4ECDC4', '#C7F464', '#FF6B6B', '#C44D58'],
-      scoreDiv: document.getElementById('score'),
-      gameBtn: document.getElementById('game-btn'),
-      speedDiv: document.getElementById('speed'),
-      speedLabel: document.getElementById('speed-label'),
-    }
+    score: 0,
+    isRunning: false,
+    interval: 0,
+    timer: 1000,
+    scoreDiv: document.getElementById('score'),
+    gameBtn: document.getElementById('game-btn'),
+    speedDiv: document.getElementById('speed'),
+    speedLabel: document.getElementById('speed-label'),
+  }
 
-  setState = (obj) => {
+  setState = obj => {
     this.state = {...this.state, ...obj}
   }
 
   init = () => {
-    let { startingScore, gameBtn, speedDiv } = this.state
+    let {startingScore, gameBtn, speedDiv} = this.state
     this.setScore(0)
     this.setSpeed()
     gameBtn.addEventListener('click', this.togglePlay)
     speedDiv.addEventListener('change', this.setSpeed)
   }
 
-  getSpeed = () => {
-    const { speedDiv } = this.state
-    return parseInt(speedDiv.value, 10)
-  }
+  getSpeed = () => parseInt(this.state.speedDiv.value, 10)
+
   setSpeed = () => {
-    const { speedLabel } = this.state
-    speedLabel.innerHTML = 'Speed: ' + this.getSpeed()
+    const {speedLabel} = this.state
+    speedLabel.innerHTML = `Speed: ${this.getSpeed()}`
   }
-  togglePlay = () => { // rename it to toggle play
-    let { gameBtn, isRunning, timer, interval } = this.state
+  togglePlay = () => {
+    let {gameBtn, isRunning, timer, interval} = this.state
     if (isRunning) {
+      Utils.updateToggleButton(gameBtn, 'Start', 'start', 'pause')
       this.setState({isRunning: false})
-      gameBtn.innerHTML = 'Start'
-      gameBtn.classList.add('start')
-      gameBtn.classList.remove('pause')
       clearInterval(interval)
     } else {
-      gameBtn.innerHTML = 'Pause'
-      gameBtn.classList.add('pause')
-      gameBtn.classList.remove('start')
+      Utils.updateToggleButton(gameBtn, 'Pause', 'pause', 'start')
       this.setState({
         isRunning: true,
-        interval: setInterval(this.play, timer)})
+        interval: setInterval(this.play, timer),
+      })
     }
   }
   play = () => {
@@ -67,7 +79,7 @@ class Game {
     this.animateDots()
   }
   addDot = () => {
-    const { colors } = this.state
+    // const {colors} = this.state
     const playground = document.getElementById('playground')
     const maxWidth = window.innerWidth - 100
     const dotSize = Utils.getRandomNum10(10, 100)
@@ -75,7 +87,7 @@ class Game {
     const dotLeftPosition = Utils.getRandomNum(0, maxWidth)
     const span = document.createElement('span')
     const topPosition = 0 - dotSize - this.getSpeed()
-  
+
     span.setAttribute('class', 'dot')
     span.setAttribute('data-size', dotSize)
     span.setAttribute('data-value', dotValue)
@@ -83,13 +95,14 @@ class Game {
     span.style.height = dotSize + 'px'
     span.style.top = topPosition + 'px'
     span.style.left = dotLeftPosition + 'px'
-    span.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)]
+    span.style['background-image'] = Gradient.getRandomGradient()
+      
     span.addEventListener('click', this.dotClicked)
     playground.appendChild(span)
   }
-  dotClicked = (event) => {
-    const { target } = event
-    const { isRunning } = this.state
+  dotClicked = event => {
+    const {target} = event
+    const {isRunning} = this.state
     const dotValue = parseInt(target.getAttribute('data-value'), 10)
     const dotWidth = target.getAttribute('data-size')
     if (isRunning) {
@@ -99,8 +112,8 @@ class Game {
       }, 0)
     }
   }
-  setScore = (value) => {
-    let { score, scoreDiv } = this.state
+  setScore = value => {
+    let {score, scoreDiv} = this.state
     const updatedScore = score + value
     this.setState({score: updatedScore})
     scoreDiv.innerHTML = `Your Score: ${updatedScore}`
@@ -109,18 +122,17 @@ class Game {
     const dots = document.querySelectorAll('.dot')
     const playgroundHeight = document.getElementById('playground').offsetHeight
     const speed = this.getSpeed()
-  
+
     for (let i = 0; i < dots.length; i++) {
       let positionY = parseInt(dots[i].style.top, 10),
         velocity = (positionY += speed)
-        if (positionY > playgroundHeight) {
+      if (positionY > playgroundHeight) {
         Utils.removeEl(dots[i])
       }
       dots[i].style.top = velocity + 'px'
     }
   }
 }
-
 
 document.addEventListener('DOMContentLoaded', () => {
   const dotGame = new Game()

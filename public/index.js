a63095620ed4f570676f3d578a4d82fcd225e1ea
Alexey Spiridonov <alexey@datacoral.co>
2019-03-10 21:56:47 -0700

    Delete eslint and prettier config files, add TODO, refactor helper.js and index.js

--- public/index.js
+++ public/index.js
@@ -1,106 +1,128 @@
-var startingScore = 0,
-    score         = 0,
-    isRunning     = false,
-    interval      = 0,
-    timer         = 1000,
-    colors        = ['#556270', '#4ECDC4', '#C7F464', '#FF6B6B', '#C44D58'],
-    scoreDiv      = document.getElementById('score'),
-    gameBtn       = document.getElementById('game-btn'),
-    speedDiv      = document.getElementById('speed'),
-    speedLabel    = document.getElementById('speed-label');
+class Utils {
+  static calcDotValue = diameter => 11 - diameter * 0.1
 
-function init() {
-  setScore(startingScore);
-  setSpeed();
-  gameBtn.addEventListener('click', toggleGame);
-  speedDiv.addEventListener('change', setSpeed);
+  static getRandomNum = (min, max) =>
+    Math.floor(Math.random() * (max - min)) + min
+
+  static getRandomNum10 = (min, max) =>
+    Math.round((Math.random() * (max - min) + min) / 10) * 10
+
+  static removeEl = el => el.parentNode.removeChild(el)
 }
 
-function getSpeed() {
-  return parseInt(speedDiv.value, 10);
-}
 
-function setSpeed() {
-  speedLabel.innerHTML = 'Speed: ' + getSpeed();
-}
+class Game {
 
-function toggleGame() {
-  if (isRunning) {
-    isRunning = false;
-    gameBtn.innerHTML = 'Start';
-    gameBtn.classList.add('start');
-    gameBtn.classList.remove('pause');
-    clearInterval(interval);
-  } else {
-    isRunning = true;
-    gameBtn.innerHTML = 'Pause';
-    gameBtn.classList.add('pause');
-    gameBtn.classList.remove('start');
-    interval = setInterval(game, timer);
-  }
-}
-
-function game() {
-  addDot();
-  animateDots();
-}
-
-function addDot() {
-  var playground      = document.getElementById('playground'),
-      maxWidth        = window.innerWidth - 100,
-      dotSize         = getRandomNum10(10, 100),
-      dotValue        = calcDotValue(dotSize),
-      dotLeftPosition = getRandomNum(0, maxWidth),
-      span            = document.createElement('span'),
-      topPosition     = 0 - dotSize - getSpeed();
-
-  span.setAttribute('class', 'dot');
-  span.setAttribute('data-size', dotSize);
-  span.setAttribute('data-value', dotValue);
-  span.style.width = dotSize + 'px';
-  span.style.height = dotSize + 'px';
-  span.style.top = topPosition  + 'px';
-  span.style.left = dotLeftPosition + 'px';
-  span.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
-  span.addEventListener('click', dotClicked);
-  playground.appendChild(span);
-}
-
-function dotClicked() {
-  var el       = this,
-      dotValue = parseInt(el.getAttribute('data-value'), 10),
-      dotWidth = el.getAttribute('data-size');
-
-  if (isRunning) {
-    setTimeout( function() {
-      setScore(dotValue);
-      removeEl(el);
-    }, 10);
-  }
-}
-
-function setScore(value) {
-  score += value;
-  scoreDiv.innerHTML = score;
-}
-
-// TODO: Play with frame rate perhaps via requestAnimationFrame ?
-function animateDots() {
-  var dots             = document.querySelectorAll('.dot'),
-      playgroundHeight = document.getElementById('playground').offsetHeight,
-      speed            = getSpeed();
-
-  for (var i = 0; i < dots.length; i++) {
-    var positionY = parseInt(dots[i].style.top, 10),
-        velocity  = positionY += speed;
-
-    if (positionY > playgroundHeight) {
-      removeEl(dots[i]);
+  state = {
+      score: 0,
+      isRunning: false,
+      interval: 0,
+      timer: 1000,
+      colors: ['#556270', '#4ECDC4', '#C7F464', '#FF6B6B', '#C44D58'],
+      scoreDiv: document.getElementById('score'),
+      gameBtn: document.getElementById('game-btn'),
+      speedDiv: document.getElementById('speed'),
+      speedLabel: document.getElementById('speed-label'),
     }
-    dots[i].style.top = velocity + "px";
+
+  setState = (obj) => {
+    this.state = {...this.state, ...obj}
+  }
+
+  init = () => {
+    let { startingScore, gameBtn, speedDiv } = this.state
+    this.setScore(0)
+    this.setSpeed()
+    gameBtn.addEventListener('click', this.togglePlay)
+    speedDiv.addEventListener('change', this.setSpeed)
+  }
+
+  getSpeed = () => {
+    const { speedDiv } = this.state
+    return parseInt(speedDiv.value, 10)
+  }
+  setSpeed = () => {
+    const { speedLabel } = this.state
+    speedLabel.innerHTML = 'Speed: ' + this.getSpeed()
+  }
+  togglePlay = () => { // rename it to toggle play
+    let { gameBtn, isRunning, timer, interval } = this.state
+    if (isRunning) {
+      this.setState({isRunning: false})
+      gameBtn.innerHTML = 'Start'
+      gameBtn.classList.add('start')
+      gameBtn.classList.remove('pause')
+      clearInterval(interval)
+    } else {
+      gameBtn.innerHTML = 'Pause'
+      gameBtn.classList.add('pause')
+      gameBtn.classList.remove('start')
+      this.setState({
+        isRunning: true,
+        interval: setInterval(this.play, timer)})
+    }
+  }
+  play = () => {
+    this.addDot()
+    this.animateDots()
+  }
+  addDot = () => {
+    const { colors } = this.state
+    const playground = document.getElementById('playground')
+    const maxWidth = window.innerWidth - 100
+    const dotSize = Utils.getRandomNum10(10, 100)
+    const dotValue = Utils.calcDotValue(dotSize)
+    const dotLeftPosition = Utils.getRandomNum(0, maxWidth)
+    const span = document.createElement('span')
+    const topPosition = 0 - dotSize - this.getSpeed()
+  
+    span.setAttribute('class', 'dot')
+    span.setAttribute('data-size', dotSize)
+    span.setAttribute('data-value', dotValue)
+    span.style.width = dotSize + 'px'
+    span.style.height = dotSize + 'px'
+    span.style.top = topPosition + 'px'
+    span.style.left = dotLeftPosition + 'px'
+    span.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)]
+    span.addEventListener('click', this.dotClicked)
+    playground.appendChild(span)
+  }
+  dotClicked = (event) => {
+    const { target } = event
+    const { isRunning } = this.state
+    const dotValue = parseInt(target.getAttribute('data-value'), 10)
+    const dotWidth = target.getAttribute('data-size')
+    if (isRunning) {
+      setTimeout(() => {
+        this.setScore(dotValue)
+        Utils.removeEl(target)
+      }, 0)
+    }
+  }
+  setScore = (value) => {
+    let { score, scoreDiv } = this.state
+    const updatedScore = score + value
+    this.setState({score: updatedScore})
+    scoreDiv.innerHTML = `Your Score: ${updatedScore}`
+  }
+  animateDots = () => {
+    const dots = document.querySelectorAll('.dot')
+    const playgroundHeight = document.getElementById('playground').offsetHeight
+    const speed = this.getSpeed()
+  
+    for (let i = 0; i < dots.length; i++) {
+      let positionY = parseInt(dots[i].style.top, 10),
+        velocity = (positionY += speed)
+        if (positionY > playgroundHeight) {
+        Utils.removeEl(dots[i])
+      }
+      dots[i].style.top = velocity + 'px'
+    }
   }
 }
 
-window.onload = function() {
-  init();
-};
+
+document.addEventListener('DOMContentLoaded', () => {
+  const dotGame = new Game()
+  dotGame.init()
+})
